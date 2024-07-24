8b3668538c1d9c3e89a6134f6670453e6514c248
zLeoAlex <z.alex.leo@outlook.com>
2024-07-24 18:44:40 +0800

    feat:  bump deps

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       {
         defaultPackage = naersk-lib.buildPackage ./.;
         devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy gnuplot ];
+          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy gnuplot linuxPackages.perf ];
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
         };
       });
